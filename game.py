4a56e9cb633895a522d6bc39e11de3156a5d48bf
HpT2 <80456375+HpT2@users.noreply.github.com>
2023-03-19 14:48:20 +0700

    test 8 stage đầu

--- game.py
+++ game.py
@@ -17,8 +17,8 @@ def readMap(fileMap):
 
 """input stage 1 là màn 1, stage2 là màn 2,... dòng 1 là vị trí khởi đầu, dòng 2 là goal. dòng 3 là số lượn switch
 các dòng tiếp theo là switch, cuối cùng là map"""
-init, goal, num_of_switches, switches = readMap("stage/stage7.txt")
-map = np.loadtxt("stage/stage7.txt",dtype=int,skiprows=3+num_of_switches)
+init, goal, num_of_switches, switches = readMap("stage/stage8.txt")
+map = np.loadtxt("stage/stage8.txt",dtype=int,skiprows=3+num_of_switches)
 
 """do numpy array đọc theo y trước x nên tọa độ phải theo thứ tự (y,x)""" 
 

f178ac2db54fc2a8ca7e285ff1d72257306c34da
HpT2 <80456375+HpT2@users.noreply.github.com>
2023-03-19 10:18:02 +0700

    complete a*, switch handle not implemented

--- Block.py
+++ Block.py
@@ -1,17 +1,18 @@
 import copy
 
 class Block:
-	def __init__(self, x, y, rotation, parent, board, x1=None,y1=None):
+	def __init__(self, x, y, rotation, parent, board, previousMove= None, x1=None,y1=None):
 		self.x = x
 		self.y = y
 		self.rotation = rotation
 		self.parent = parent
 		self.board = copy.deepcopy(board)
+		self.previousMove = previousMove
 		self.x1 = x1
 		self.y1 = y1
 
 	def UP(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "UP")
 		if self.rotation == "STANDING":
 			newBlock.y -= 2
 			newBlock.rotation = "LAYING_Y"
@@ -23,7 +24,7 @@ class Block:
 		return newBlock
 	
 	def DOWN(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board,"DOWN")
 		if newBlock.rotation == "STANDING":
 			newBlock.y += 1
 			newBlock.rotation = "LAYING_Y"
@@ -35,7 +36,7 @@ class Block:
 		return newBlock
 	
 	def LEFT(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "LEFT")
 		if newBlock.rotation == "STANDING":
 			newBlock.x -= 2
 			newBlock.rotation = "LAYING_X"
@@ -47,7 +48,7 @@ class Block:
 		return newBlock
 	
 	def RIGHT(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "RIGHT")
 		if newBlock.rotation == "STANDING":
 			newBlock.x += 1
 			newBlock.rotation = "LAYING_X"
@@ -59,48 +60,68 @@ class Block:
 		return newBlock
 	
 	def SPLIT_UP(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board, self.x1, self.y1)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "SPLIT 0 UP", self.x1, self.y1)
 		newBlock.y -= 1
 		return newBlock
 	
 	def SPLIT_DOWN(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board, self.x1, self.y1)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "SPLIT 0 DOWN", self.x1, self.y1)
 		newBlock.y += 1
 		return newBlock
 	
 	def SPLIT_LEFT(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board, self.x1, self.y1)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "SPLIT 0 LEFT", self.x1, self.y1)
 		newBlock.x -= 1
 		return newBlock
 	
 	def SPLIT_RIGHT(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board, self.x1, self.y1)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "SPLIT 0 RIGHT", self.x1, self.y1)
 		newBlock.x += 1
 		return newBlock
 	
 	def SPLIT_UP_1(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board, self.x1, self.y1)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "SPLIT 1 UP", self.x1, self.y1)
 		newBlock.y1 -= 1
 		return newBlock
 	
 	def SPLIT_DOWN_1(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board, self.x1, self.y1)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "SPLIT 1 DOWN", self.x1, self.y1)
 		newBlock.y1 += 1
 		return newBlock
 	
 	def SPLIT_LEFT_1(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board, self.x1, self.y1)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "SPLIT 1 LEFT", self.x1, self.y1)
 		newBlock.x1 -= 1
 		return newBlock
 	
 	def SPLIT_RIGHT_1(self):
-		newBlock = Block(self.x, self.y, self.rotation, self, self.board, self.x1, self.y1)
+		newBlock = Block(self.x, self.y, self.rotation, self, self.board, "SPLIT 1 RIGHT", self.x1, self.y1)
 		newBlock.x1 += 1
 		return newBlock
 	
 	def isGoal(self):
 		# statements
-		if self.rotation == "STANDING" and self.board[self.y][self.x] == 9:
-			return True
-		else:
+		try:
+			if self.rotation == "STANDING" and self.board[self.y][self.x] == 9:
+				return True
+			else:
+				return False
+		except:
 			return False
+		
+	def GameOver(self):
+		try:
+			if self.rotation == "STANDING" and self.board[self.y][self.x] == 0 :
+				return True
+			if self.rotation == "LAYING_Y" and (self.board[self.y][self.x] == 0 or self.board[self.y+1][self.x] == 0 ):
+				return True
+			if self.rotation == "LAYING_X" and (self.board[self.y][self.x] == 0 or self.board[self.y][self.x+1] == 0 ):
+				return True
+			if self.rotation == "SPLIT" and self.board[self.y1][self.x1] == 0:
+				return True
+		except :
+			return True
+		return False
+	
+	def isSwitch(self):	
+		pass
--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-from Stage import Stage
 import numpy as np
 from Block import Block
 import A_star
@@ -7,28 +6,24 @@ import os
 import psutil
 
 """input stage 1 là màn 1, stage2 là màn 3, dòng 1 là vị trí khởi đầu, dòng 2 là goal"""
-with open("stage2.txt") as file:
+with open("stage/stage6.txt") as file:
 	init = [int(x) for x in file.readline().split(' ')]
 	goal = [int(x) for x in file.readline().split(' ')]
 
 
-map = np.loadtxt("stage2.txt",dtype=int,skiprows=2)
-stage = Stage(map,goal)
+map = np.loadtxt("stage/stage6.txt",dtype=int,skiprows=2)
 
-"""do numpy array đọc theo y trước x nên tọa độ phải theo thứ tự (y,x)"""
-pos1 = [init[0],init[1]] 
-pos2 = [init[2],init[3]]
-block = Block(pos1,pos2)
-block1 = Block(pos1,pos2)
+"""do numpy array đọc theo y trước x nên tọa độ phải theo thứ tự (y,x)""" 
+block = Block(init[1], init[0], "STANDING", None, map)
 
 
-print('Goal: {}'.format(stage.goal))
-print("block: [{},{}] [{},{}]".format(block.pos1.y, block.pos1.x, block.pos2.y, block.pos2.x))
-print("Map:\n{}".format(stage.map))
+print('Goal: {}'.format(goal))
+print("block: [{}, {}]".format(block.y, block.x))
+print("Map:\n{}".format(map))
 
 """Test"""
 start = timeit.default_timer()
-finish_node = A_star.solve(stage,block)
+finish_node = A_star.solve(block, goal)
 stop = timeit.default_timer()
 
 print("A-star take: " + str(round(stop - start, 4)) + " s")
@@ -36,14 +31,14 @@ print("A-star take: " + str(round(stop - start, 4)) + " s")
 process = psutil.Process(os.getpid())
 
 print('Memory usage: ' + str(round(process.memory_info().rss / (1024 * 1024), 2)) + " MB")
-
-
-
-last = finish_node
-path = []
-while(last.parent):
-	path = [last.block.previousMove] + path
-	#print('[{} {}] [{} {}] {}'.format(last.block.pos1.y,last.block.pos1.x,last.block.pos2.y, last.block.pos2.x,last.block.previousMove))
-	last = last.parent
-print(len(path))
-print(path)
+if(finish_node):
+	last = finish_node
+	path = []
+	while(last.parent):
+		path = [last.previousMove] + path
+		#print('[{} {}] [{} {}] {}'.format(last.block.pos1.y,last.block.pos1.x,last.block.pos2.y, last.block.pos2.x,last.block.previousMove))
+		last = last.parent
+	print(len(path))
+	print(path)
+else:
+	print("FAILED")

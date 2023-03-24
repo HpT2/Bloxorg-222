3a8365366ccd427f4030e46b1ce4286215f181a8
HpT2 <80456375+HpT2@users.noreply.github.com>
2023-03-24 23:19:09 +0700

    optimized monte carlo

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -38,14 +38,27 @@ class Node:
 		self.children = [Node(block, self) for block in make_move(self.block) if block.isValidBlock()]
 
 	def simulate(self):
-		block_ = self.block
 		i = 0
-		while not block_.isGoal():
-			move = [block for block in make_move(block_) if block.isValidBlock()]
-			block_ = random.choice(move)
+		queue = []
+		queue.append(self.block)
+		close = []
+		while queue:
+			x = random.randint(0,len(queue)-1)
+			current = queue.pop(x)
+
+			if current.isGoal():
+				return -i
+
+			if current in close:
+				continue
+			
+			close.append(current)
+
+			newBlocks = [block for block in make_move(current) if block.isValidBlock()]
+			for block in newBlocks:
+				queue.append(block)
 			i += 1
 			#print(block_.previousMove)
-		return  -i
 	
 	def backpropagate(self, score):
 		node = self
@@ -63,6 +76,7 @@ def MonteCarlo(root, max_ite, examined):
 		#selection
 		while node.children:
 			node = node.select()
+			#print(node.block.previousMove)
 
 
 			
@@ -73,7 +87,7 @@ def MonteCarlo(root, max_ite, examined):
 
 		#simulation
 		score = node.simulate()
-
+		#print(score)
 		#backpropagation
 		node.backpropagate(score)
 	best = max(root.children, key=lambda child: child.visits)
@@ -94,9 +108,11 @@ def solve(block):
 	examined = []
 	i = 0
 	while not node.block.isGoal():
-		node = MonteCarlo(node, 1000, examined)
+		node = MonteCarlo(node, 10, examined)
 		if node.block not in examined:
 			examined.append(node.block)
+		
+		#print(node.block.previousMove)
 
 
 		#print(node.block.previousMove)
